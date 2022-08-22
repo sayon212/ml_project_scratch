e3b6ecceb87650be6deb3a61b8476852c256a072
Sayon <sayon.biems@gmail.com>
2022-08-22 17:21:17 +0530

    release 2

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/" , methods = ['GET' , 'POST'])
 def index():
-    return "Starting ML Project"
+    return "CI CD Pipeline Created for ML Project"
 
 if __name__ == "__main__":
     app.run()
