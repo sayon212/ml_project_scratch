c1df954b71c75bbb420b576806ed0ef5982dc580
Sayon <sayon.biems@gmail.com>
2022-09-10 15:06:23 +0530

    Exception added

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from flask import Flask
+from housing.logger import logging
+from housing.exception import HousingException
+import sys
 
 app = Flask(__name__)
 
+logging.info("Testing Logger")
+
 @app.route("/" , methods = ['GET' , 'POST'])
 def index():
+    try:
+        raise Exception("We are testing Exception...")
+    except Exception as e:
+        housing_exception = HousingException(e,sys)
+        logging.info(housing_exception.error_message)
     return "CI CD Pipeline Created for ML Project"
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
